43bb9b454212a8db9858cc2b687a1979912ecd71
Benit Kibabu <x08424179@student.ncirl.ie>
2017-03-24 16:13:23 +0000

    update

--- OnlineDatabaseEx/app/src/main/java/com/kibabu/benit/onlinedatabaseex/LoginActivity.java
+++ OnlineDatabaseEx/app/src/main/java/com/kibabu/benit/onlinedatabaseex/LoginActivity.java
@@ -1,23 +1,104 @@
 package com.kibabu.benit.onlinedatabaseex;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.kibabu.benit.onlinedatabaseex.app.AppController;
+import com.kibabu.benit.onlinedatabaseex.helper.AppPreference;
+import com.kibabu.benit.onlinedatabaseex.helper.DbHelper;
 import com.kibabu.benit.onlinedatabaseex.model.User;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LoginActivity extends AppCompatActivity {
+    private static final String APIULR = "http://virtualkitchen.gear.host/?tag=login";
 
     Button login, register;
     EditText email, password;
 
+
     //User Model
     User u;
+
+    DbHelper db;
+
+    AppPreference pref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        login = (Button) findViewById(R.id.login_button);
+        register = (Button ) findViewById(R.id.register_button);
+
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+
+        db = new DbHelper(this);
+        pref  = new AppPreference(this);
+
+
+        login("benitkibabu@gmail.com", "123456");
     }
+
+    void login(final String email, final String password){
+
+        final String tag = "login";
+
+        Uri url = Uri.parse(APIULR)
+                .buildUpon()
+                .appendQueryParameter("tag", tag)
+				.appendQueryParameter("email", email)
+				.appendQueryParameter("password", password)
+                .build();
+
+        StringRequest request = new StringRequest(
+                Request.Method.GET,
+                url.toString(), new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response){
+                Log.w("Login Activity", response);
+            }
+        },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Login Activity", error.getMessage());
+                    }
+                }
+        ){
+           /* @Override
+            protected Map<String, String> getParams() throws AuthFailureError{
+                Map<String, String> params = new HashMap<>();
+                params.put("tag", tag);
+                params.put("email", email);
+                params.put("password", password);
+
+                return params;
+            }*/
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError{
+                Map<String, String> params = new HashMap<>();
+
+                return params;
+            }
+        };
+
+
+        AppController.getInstance().addToRequestQueue(request, tag);
+
+    }
+
 }
--- OnlineDatabaseEx/app/src/main/java/com/kibabu/benit/onlinedatabaseex/helper/AppPrefereence.java
+++ OnlineDatabaseEx/app/src/main/java/com/kibabu/benit/onlinedatabaseex/helper/AppPrefereence.java
@@ -1,8 +1,64 @@
 package com.kibabu.benit.onlinedatabaseex.helper;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+
 /**
- * Created by Ben on 20/03/2017.
+ * Created by scr3 on 15/03/2017.
  */
 
-public class AppPrefereence {
+public class AppPreference {
+
+    Context context;
+    SharedPreferences pref;
+    Editor editor;
+
+    int PRIVATE_MODE = 0;
+
+    private static final String PREF_NAME = "SAMPLELOGIN";
+    private static final String KEY_IS_LOGIN = "isLogin";
+    private static final String KEY_USER = "username";
+    private static final String KEY_USER_PASS = "password";
+
+    public AppPreference(Context context){
+        this.context = context;
+        pref = this.context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+    }
+
+    public void setKeyIsLogin(String tag, boolean value){
+        editor = pref.edit();
+        editor.putBoolean(tag, value);
+        editor.apply();
+    }
+
+    public void setUserName(String val){
+        editor = pref.edit();
+        editor.putString(KEY_USER, val);
+        editor.apply();
+    }
+    public void setUserPass(String val){
+        editor = pref.edit();
+        editor.putString(KEY_USER_PASS, val);
+        editor.apply();
+    }
+
+    public String getUser() {
+
+        return pref.getString(KEY_USER, "null");
+    }
+
+    public String getUserPass() {
+        return pref.getString(KEY_USER_PASS, "null");
+    }
+
+    public boolean getIsLogin(){
+        boolean val = pref.getBoolean(KEY_IS_LOGIN, false);
+        return val;
+    }
+
+    public boolean getIsLogin(String tag){
+        boolean val = pref.getBoolean(tag, false);
+        return val;
+    }
 }
